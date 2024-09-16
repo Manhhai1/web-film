e95dfbec43e128b4732bc4bd7bee09049ccd4724
unknown <vomanhhai230720@.com>
2024-09-16 11:01:44 +0700

    seo web

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-// next.config.js
 module.exports = {
     images: {
       domains: ['phimimg.com'], // Thêm tên miền phimimg.com vào đây
     },
   };
   
-export default nextConfig;
--- src/components/movies/MovieCard.js
+++ src/components/movies/MovieCard.js
@@ -1,27 +1,41 @@
 // src/components/movies/MovieCard.js
-'use client'
-import './MovieCard.css'
-import play_icon from '../../assets/img/icons8-play-48.webp'
+'use client';
+import './MovieCard.css';
+import play_icon from '../../assets/img/icons8-play-48.webp';
 import Link from 'next/link';
-const checkString = (s) => {
-  let str = 'https://phimimg.com/'
-  if (s?.includes(str)) return s
-  else {
-    s = 'https://phimimg.com/' + s
-    return s
-  }
-}
-const MovieCard = ({ movie}) => {
-    return (
-       <Link href={`/infor/${movie?.slug}`||'#'}> <div className="item" key={movie?._id}>
-        <span className='label'>{movie?.lang || 'Vietsub'}-{movie?.quality || 'HD'}</span>
-                <img className='item-img' src={`${checkString(movie?.poster_url)}`} />
-                <h1 className='name-film'>{movie?.name}</h1>
-                <img className='play-icon' src={play_icon.src} alt="" />
-              </div>
-      </Link>
-    );
-  };
+import Image from 'next/image';
 
-  export default MovieCard;
-  
+const checkString = (s) => {
+  const baseUrl = 'https://phimimg.com/';
+  return s?.includes(baseUrl) ? s : baseUrl + s;
+};
+
+const MovieCard = ({ movie }) => {
+  return (
+    <Link href={`/infor/${movie?.slug || '#'}`}>
+      <div className="item" key={movie?._id}>
+        <span className='label'>{movie?.lang || 'Vietsub'} - {movie?.quality || 'HD'}</span>
+        <Image
+          className='item-img'
+          src={checkString(movie?.poster_url)}
+          alt={movie?.name || 'Movie Poster'}
+          width={230} 
+          height={350}
+          loading="lazy"
+        />
+        
+        <h1 className='name-film'>{movie?.name}</h1>
+        
+        <Image
+          className='play-icon'
+          src={play_icon.src}
+          alt="Play Icon"
+          width={48} 
+          height={48}
+        />
+      </div>
+    </Link>
+  );
+};
+
+export default MovieCard;
