59b32068d0275033ec830ea43b481c1c0c638238
unknown <vomanhhai230720@.com>
2024-09-16 13:28:52 +0700

    seo web

--- src/app/robots.js
+++ src/app/robots.js
@@ -4,7 +4,6 @@ export default function robots() {
       {
         userAgent: '*',
         allow: '/', // Explicitly allow all other pages
-        disallow: ['/include/', '/tim-kiem/', '/tag/'] // Disallow specific paths
       }
     ],
     sitemap: 'https:/top-phim.pro/sitemap.xml'
